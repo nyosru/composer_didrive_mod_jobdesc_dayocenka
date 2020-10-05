2ec67a3d94e562cd058c660ee5e1ccee7e211f77
Сергей <nyos@rambler.ru>
2020-10-05 06:04:57 +0500

    1.1.0

--- src/class.php
+++ src/class.php
@@ -63,4 +63,68 @@ class JOBDESC_DAYOCENKA {
         return $return;
     }
 
+    /**
+     * на какие даты нет оценок у точек продаж
+     * на выходе массив = дата - точки
+     * @param type $db
+     */
+    public static function whereBlankOcenka($db, $scan_day = 60 ) {
+
+        \Nyos\mod\items::$sql_select_vars[] = 'id';
+        $sps = \Nyos\mod\items::get($db, \Nyos\mod\JobDesc::$mod_sale_point, 'show', 'id_id');
+        // $sps = array_keys($sps0);
+        // \f\pa($sps);
+
+        $return_sp_date = [];
+
+        try {
+
+            $s2 = $db->prepare('SELECT `sale_point`, `date` FROM `mod_' . \f\translit(\Nyos\mod\JobDesc::$mod_ocenki_days, 'uri2') . '` WHERE `status` = \'show\' AND `date` >= :date ;');
+            $date = date('Y-m-d', $_SERVER['REQUEST_TIME'] - 3600 * 24 * $scan_day );
+            // \f\pa($date);
+            $s2->execute([':date' => $date]);
+
+//            $r2 = $s2->fetchAll();
+//            \f\pa($r2);
+
+            while ($r = $s2->fetch()) {
+                $return_sp_date[$r['sale_point']][$r['date']] = 1;
+            }
+
+
+            $return_date_sp = [];
+
+            $ndate = '';
+
+            for ($t = 1; $t <= ( $scan_day + 2 ) ; $t++) {
+
+                $ndate = date('Y-m-d', $_SERVER['REQUEST_TIME'] - $t * 3600 * 24);
+
+                // echo '<br/>'.$ndate;
+
+                foreach ($sps as $sp => $v) {
+
+                    // echo '<br/>++ '.$sp;
+
+                    if (!isset($return_sp_date[$sp][$ndate])) {
+                        // echo ' 0 ';
+                        $return_date_sp[$ndate][$sp] = 1;
+                    }
+                }
+            }
+
+            // \f\pa($return_sp_date);
+
+            return $return_date_sp;
+        } catch (\PDOException $ex) {
+            // \f\pa($ex);
+            \nyos\Msg::sendTelegramm(__FILE__ . ' #' . __LINE__ . ' ошибка ' . $ex->getMessage(), null, 2);
+        } catch (\Exception $ex) {
+            // \f\pa($ex);
+            \nyos\Msg::sendTelegramm(__FILE__ . ' #' . __LINE__ . ' ошибка ' . $ex->getMessage(), null, 2);
+        }
+
+        return false;
+    }
+
 }
